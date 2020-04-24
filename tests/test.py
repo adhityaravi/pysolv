0927072027866819df53bf216e7f65f18aa5a646
aravinth_spicer <aravinth18ravi@gmail.com>
2020-04-24 13:23:43 +0200

    gauss-seidel-scheme

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,19 @@
 import sys
-sys.path.append('/home/adhitya/pysolv')
+sys.path.append('/home/aravinth/pysolv')
 
 import pysolv
 import numpy as np
 
 #print(pysolv.__version__)
 
-A = [[2, 1], [5, 7]]
+A = [[1, 0, 1], [0, -3, 1], [2, 1, 3]]
 A = np.array(A)
 
-b = [11, 13]
+b = [6, 7, 15]
 b = np.array(b)
 
 x = pysolv.solve(A, b, 'Jacobi')
+print(x)
 
+x = pysolv.solve(A, b, 'Gaussseidel')
 print(x)
