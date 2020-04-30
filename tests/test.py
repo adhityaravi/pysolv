8da8ee916ec2eeb213335bc6395b1a5f4991dc74
adhityaravi <adhityashan95@gmail.com>
2020-04-30 16:18:06 +0200

    [1-jacobi-scheme-2] Method nomenclature modification

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import pysolv
 import numpy as np
+import time as ti
 
 #print(pysolv.__version__)
 
@@ -13,14 +14,15 @@ A = np.array(A)
 b = [6, 7, 15]
 b = np.array(b)
 
-# x = pysolv.solve(A, b, 'Jacobi')
-# print(x)
+x = pysolv.solve(A, b, 'Jacobi', omega=0.8)
+print(x)
 #
 # x = pysolv.solve(A, b, 'Gauss-Seidel')
 # print(x)
 
-x, *_ = pysolv.solve(A, b, 'SOR')
+x = pysolv.solve(A, b, 'SOR', omega=1.3)
 print(x)
 
-x, *_ = pysolv.solve(A, b, 'SSOR', omega=1.3)
+x = pysolv.solve(A, b, 'SSOR', omega=1.3)
 print(x)
+
